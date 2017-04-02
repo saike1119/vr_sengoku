338b0ea223f1619395e22a13637cd89a19b31070
saizoukun <saike1119@gmail.com>
2017-04-03 00:50:17 +0900

    勝利と敗北シーン、スクリプト、遷移追加

---
+++ Assets/Scripts/SceneStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (DelayScene ());
+	}
+
+	IEnumerator DelayScene(){
+		yield return new WaitForSeconds (7.0f);
+		SceneManager.LoadScene ("StartScene");
+	}
+}
